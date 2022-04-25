30a672c88da955910afea57798b3af7d70b6fa10
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-04-25 15:32:23 +0300

    Creating Edit Button functionality

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ async function getUsers(url) {
         }
         const users = await response.json();
         users.forEach(user => {
-            console.log(user);
             createTableElements(user);
         });
     } catch(error) {
@@ -19,37 +18,78 @@ async function getUsers(url) {
 function createTableElements(userInfo) {
     //create the table elements
     const table = document.getElementById('users');
-    const tr = document.createElement('tr');
+    const tableRow = document.createElement('tr');
     const firstName = document.createElement('td');
     const lastName = document.createElement('td');
     const address = document.createElement('td');
+    const action = document.createElement('td');
+   
     //split userInfo.name into first and last name
     const name = userInfo.name.split(' ');
     firstName.textContent = name[0];
     lastName.textContent = name[1];
     address.textContent = userInfo.address.street;
-    tr.appendChild(firstName);
-    tr.appendChild(lastName);
-    tr.appendChild(address);
+    tableRow.appendChild(firstName);
+    tableRow.appendChild(lastName);
+    tableRow.appendChild(address);
 
+    //edit button
     const editBtn = document.createElement('button');
+    editBtn.classList.add('button', 'edit-btn');
     editBtn.textContent = 'Edit';
     editBtn.addEventListener('click', () => {
-        editUser(userInfo);
+       editUserHiddenPopupForm(userInfo);
     });
-    tr.appendChild(editBtn);
-
+    action.appendChild(editBtn);    
+    
+    //delete button
     const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('button', 'delete-btn');
     deleteBtn.textContent = 'Delete';
     deleteBtn.addEventListener('click', () => {
         deleteUser(userInfo);
     });
-    tr.appendChild(deleteBtn);
+    action.appendChild(deleteBtn);
+    
+    tableRow.appendChild(action);
 
-    table.appendChild(tr);
+    table.appendChild(tableRow);
 }
 
+function editUserHiddenPopupForm(userInfo) {
+    const formPopup = document.createElement('div');
+    formPopup.classList.add('form-popup');
+    const formContainer = document.createElement('form');
+    formContainer.classList.add('form-container');
+    
+    const firstNameInput = document.createElement('input');
+    firstNameInput.type = 'text';
+    firstNameInput.name = 'firstname';
+    firstNameInput.value = userInfo.name.split(' ')[0];
+    
+    const lastNameInput = document.createElement('input');
+    lastNameInput.type = 'text';
+    lastNameInput.name = 'lastname';
+    lastNameInput.value = userInfo.name.split(' ')[1];
+   
+    const addressInput = document.createElement('input');
+    addressInput.type = 'text';
+    addressInput.name = 'address';
+    addressInput.value = userInfo.address.street;
+   
+    const saveBtn = document.createElement('button');
+    saveBtn.classList.add('button', 'save-btn');
+    saveBtn.textContent = 'Save';
+    saveBtn.addEventListener('click', () => {
+        const firstName = firstNameInput.value;
+        const lastName = lastNameInput.value;
+        const address = addressInput.value;
+    });
 
+    formContainer.append(firstNameInput, lastNameInput, addressInput, saveBtn);
+    formPopup.appendChild(formContainer);
+    document.body.appendChild(formPopup);
+}
 
 
 getUsers('https://jsonplaceholder.typicode.com/users');

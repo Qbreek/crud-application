67a0c46b460bcd8a688aea67e551b857a23421d5
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-04-25 14:20:43 +0300

    Made a function that creates the table elements using the fetch data

--- script.js
+++ script.js
@@ -8,10 +8,48 @@ async function getUsers(url) {
         const users = await response.json();
         users.forEach(user => {
             console.log(user);
+            createTableElements(user);
         });
     } catch(error) {
         console.error(`Could not get users: ${error}`);
     }
 }
+
+//for every user create the coresponding table element
+function createTableElements(userInfo) {
+    //create the table elements
+    const table = document.getElementById('users');
+    const tr = document.createElement('tr');
+    const firstName = document.createElement('td');
+    const lastName = document.createElement('td');
+    const address = document.createElement('td');
+    //split userInfo.name into first and last name
+    const name = userInfo.name.split(' ');
+    firstName.textContent = name[0];
+    lastName.textContent = name[1];
+    address.textContent = userInfo.address.street;
+    tr.appendChild(firstName);
+    tr.appendChild(lastName);
+    tr.appendChild(address);
+
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.addEventListener('click', () => {
+        editUser(userInfo);
+    });
+    tr.appendChild(editBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+        deleteUser(userInfo);
+    });
+    tr.appendChild(deleteBtn);
+
+    table.appendChild(tr);
+}
+
+
+
 
 getUsers('https://jsonplaceholder.typicode.com/users');
